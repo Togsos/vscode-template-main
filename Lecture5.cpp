1898fd02a08c02402acaabe26882dec377c21e75
Togsos <115413036+Togsos@users.noreply.github.com>
2022-10-11 18:53:36 +0100

    first Commit

---
+++ Lecture5.cpp
@@ -0,0 +1,16 @@
+#include "raylib.h"
+
+int main(){
+    const int windowWidth{500};
+    const int windowHeight{500};
+    InitWindow(windowWidth,windowHeight, "Sharon's Window");
+
+    SetTargetFPS(60);
+    while(!WindowShouldClose()){
+
+        BeginDrawing();
+        ClearBackground(WHITE);
+        EndDrawing();
+    }
+    CloseWindow();
+}
