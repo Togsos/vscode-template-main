2ad9899a94790ade4b89726088fd6483852bdf1c
Togsos <D00262376@student.dkit.ie>
2022-11-06 20:01:22 +0000

    thirdCommit

--- Practice.cpp
+++ Practice.cpp
@@ -4,16 +4,29 @@
 int main()
 
 {
-   int width {800};
-   int height {750};
+   const int screenWidth {800};
+   const int screenHeight {750};
+   char WindowName[] = "Plague Doctor";
    
-InitWindow(width, height,"Sharon's Window");
+InitWindow(screenWidth, screenHeight, WindowName);
+  const char msg[50] = "Merrie Olde England 1349...";
+   Font font = LoadFont("resources/custom_alagard.png");   
+   Vector2 fontPosition = { screenWidth/2.0f - MeasureTextEx
+   (font, msg, (float)font.baseSize, -3).x/2,screenHeight/2.0f - font.baseSize/2.0f - 80.0f };
+
+SetTargetFPS(60);
+
 while(WindowShouldClose() == false)
+
 {
 BeginDrawing();
-ClearBackground(RED);
+   ClearBackground(WHITE);
+   DrawTextEx(font, msg, fontPosition, (float)font.baseSize, -3, RED);
 EndDrawing();
 } 
+UnloadFont(font);
+
+return 0;
 }
 
 
